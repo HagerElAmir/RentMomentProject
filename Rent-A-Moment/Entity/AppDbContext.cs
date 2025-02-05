db92b4c23ffab101221ee5935f5d7359b5345627
HagerElAmir <hagerashraf784@gmail.com>
2025-02-05 23:15:30 +0200

    hajer

--- Rent-A-Moment/Entity/AppDbContext.cs
+++ Rent-A-Moment/Entity/AppDbContext.cs
@@ -14,12 +14,6 @@ namespace Rent_A_Moment.Entity
 
         }
         public DbSet<Admin> Admins { get; set; }
-
-
-
-
-
-
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
